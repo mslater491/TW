c52c2dd7ff9d1b109542bf5f9b134a509bd52ca7
CORE\Phil Manning <night_gia@hotmail.com>
2018-10-07 18:30:22 -0700

    Initial Card and (temp) Player scripting
    Created a card prefab sample.
    Created a script to attach to card prefab that holds data.
    Created aplayer script to attach to player prefab (mostly as just a temporary holder/planning).
    Created sample CSV with two test cards.
    Created a CardOverseer script to handle card data and load it from the CSV.

---
+++ TWProject/Assets/Scripts/CSVOperation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CSVOperations : MonoBehaviour
+{
+    public static string[] Load(string Table)
+    {
+        TextAsset rawData = Resources.Load("CSV/" + Table, typeof(TextAsset)) as TextAsset;
+        if (rawData == null)
+        {
+            Debug.LogError("CSV read error.");
+            return null;
+        }
+        else
+        {
+            return rawData.text.Split('\n');
+        }
+    }
+}
